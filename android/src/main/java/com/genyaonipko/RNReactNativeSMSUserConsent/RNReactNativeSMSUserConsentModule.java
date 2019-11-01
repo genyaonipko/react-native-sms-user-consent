56ca42520affa80966ff93504c1581489f580146
Yevhenii Onipko <onipko32@gmail.com>
2019-11-01 15:10:00 +0200

    refactor

--- android/src/main/java/com/genyaonipko/RNReactNativeSMSUserConsent/RNReactNativeSMSUserConsentModule.java
+++ android/src/main/java/com/genyaonipko/RNReactNativeSMSUserConsent/RNReactNativeSMSUserConsentModule.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -17,6 +18,7 @@ import com.google.android.gms.auth.api.phone.SmsRetriever;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.Status;
 
+
 public class RNReactNativeSMSUserConsentModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
@@ -24,19 +26,15 @@ public class RNReactNativeSMSUserConsentModule extends ReactContextBaseJavaModul
     public RNReactNativeSMSUserConsentModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        reactContext.addActivityEventListener(activityEventListener);
+        SmsRetriever.getClient(reactContext).startSmsUserConsent(null);
+        IntentFilter intentFilter = new IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION);
+        reactContext.registerReceiver(smsVerificationReceiver, intentFilter);
     }
 
     @Override
     public String getName() {
-        return "RNReactNativeSMSUserConsentModule";
-    }
-
-
-    @ReactMethod
-    public void addMessageListener() {
-        SmsRetriever.getClient(reactContext).startSmsUserConsent(null);
-        IntentFilter intentFilter = new IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION);
-        reactContext.registerReceiver(smsVerificationReceiver, intentFilter);
+        return "RNReactNativeSMSUserConsent";
     }
 
     private static final int SMS_CONSENT_REQUEST = 2;  // Set to an unused request code
@@ -64,6 +62,29 @@ public class RNReactNativeSMSUserConsentModule extends ReactContextBaseJavaModul
                         break;
                 }
 
+            }
+        }
+    };
+
+    private final ActivityEventListener activityEventListener = new ActivityEventListener() {
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            switch (requestCode) {
+                // ...
+                case SMS_CONSENT_REQUEST:
+                    if (resultCode == Activity.RESULT_OK) {
+                        // Get SMS message content
+                        String message = data.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE);
+                        // Extract one-time code from the message and complete verification
+                        // `sms` contains the entire text of the SMS message, so you will need
+                        // to parse the string.
+//                        String oneTimeCode = parseOneTimeCode(message); // define this function
+
+                        // send one time code to the server
+                    } else {
+                        // Consent canceled, handle the error ...
+                    }
+                    break;
             }
         }
     };
