7209cd4c66b05d3d6805048789b344cd885bea50
Yevhenii Onipko <onipko32@gmail.com>
2019-11-01 14:50:49 +0200

    refactor

--- index.js
+++ index.js
@@ -2,26 +2,33 @@ import { NativeModules, Platform } from "react-native";
 const { RNReactNativeSMSUserConsent } = NativeModules;
 
 export default class SMSUserConsent {
-  static async addMessageListener() {
+  static addMessageListener() {
+    return new Promise((resolve, reject) => {
+      try {
+        if (Platform.OS != "ios" && Platform.OS != "android") {
+          reject(Platform.OS);
+        }
+
+        RNReactNativeSMSUserConsent.addMessageListener();
+        resolve("success");
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+  static getOneTimeCode() {
     return new Promise((resolve, reject) => {
       if (Platform.OS != "ios" && Platform.OS != "android") {
         reject(Platform.OS);
       }
 
-      RNReactNativeSMSUserConsent.addMessageListener();
-      return resolve('success')
+      RNReactNativeSMSUserConsent.getOneTimeCode((errorCode, item) => {
+        if (errorCode != null) {
+          reject(errorCode);
+        } else {
+          resolve(item);
+        }
+      });
     });
-  }
-  static async getOneTimeCode() {
-    return new Promise((resolve, reject)=>{
-      if ((Platform.OS != 'ios') && (Platform.OS != 'android')) {
-        reject(Platform.OS)
-      }
-
-      const oneTimeCode = RNReactNativeSMSUserConsent.getOneTimeCode();
-      if (oneTimeCode) {
-        resolve(oneTimeCode)
-      }
-    })
   }
 }
